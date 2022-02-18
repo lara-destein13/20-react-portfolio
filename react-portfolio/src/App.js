91ef0bf8275832b370f97ea153fcf0acfa365a79
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-18 18:58:46 -0500

    create the app component

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
+import autoBind from 'auto-bind';
+import React, { Component } from 'react';
+// import AboutMe from './AboutMe';
+// import ContactMe from './ContactMe';
+// import Footer from './Footer';
+// import Header from './Header';
+// import Portfolio from './Portfolio';
+// import Resume from './Resume';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Componenet {
+  // constructor
+  constructor(props) {
+    super(props);
+    autoBind(this);
+
+    this.state = {
+      middle: 'ABOUT_ME'
+    }
+  }
+
+  // renderAboutMe
+  renderAboutMe() {
+    if (this.state.middle !== 'ABOUT_ME') {
+      return null;
+    }
+    return( <div>contactMe</div> );
+  }
+
+  // renderFooter
+  renderFooter() {
+    return( <div>footer</div> );
+  }
+
+  // renderPortfolio
+  renderPortfolio() {
+    if (this.state.middle !== 'PORTFOLIO') {
+      return null;
+    }
+    return( <div>aportfolio</div> );
+  }
+
+  // renderResume
+  renderResume() {
+    if (this.state.middle !== 'RESUME') {
+      return null;
+    }
+    return( <div>resume</div> );
+  }
+
+  // render
+  render() {
+    return (
+      <div>
+        { this.renderHeader() }
+        { this.renderAboutMe() }
+        { this.renderContactMe() }
+        { this.renderPortfolio() }
+        { this.renderResume() }
+        { this.renderFooter() }
+      </div>
+    );
+  }
+
 }
 
 export default App;
