3580aa8bc83ec20362a5ba83cdc793f6b060410f
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-02-20 18:17:20 -0500

    work on portfolio componenet, add screenshots

--- react-portfolio/src/Portfolio.js
+++ react-portfolio/src/Portfolio.js
@@ -1,6 +1,13 @@
 import autoBind from 'auto-bind';
 import React, { Component } from 'react';
 import './Portfolio.css';
+import password from './password-generator.png';
+import quiz from './code-quiz.png';
+import scheduler from './work-day.png';
+import weather from  './weather.png';
+import trails from './backyard-trails.png';
+import stick from './stick-ease.png';
+
 
 class Portfolio extends Component {
     // constructor
@@ -16,42 +23,36 @@ class Portfolio extends Component {
     render() {
         return (
             <div className="Portfolio">  
-                <div className="projects"> 
-                    <a className="port-link" href="https://lara-destein13.github.io/password-generator/">
-                        <h2 className="project-title">Password Generator</h2>
-                        <p className="project-description">(Javascript)</p>
-                    </a>
-                </div>
-                <div className="projects"> 
-                    <a className="port-link" href="https://lara-destein13.github.io/4-code-quiz/">
-                        <h2 className="project-title">Code Quiz</h2>
-                        <p className="project-description">(Web APIs)</p>
-                    </a>
-                </div>
-                <div className="projects"> 
-                    <a className="port-link" href="https://lara-destein13.github.io/5-work-day-scheduler/">
-                        <h2 className="project-title">Work Day Scheduler</h2>
-                        <p className="project-description">(Third-Party APIs)</p>
-                    </a>
-                </div>
-                <div className="projects"> 
-                    <a className="port-link" href="https://lara-destein13.github.io/weather/">
-                        <h2 className="project-title">Weather Dashboard</h2>
-                        <p className="project-description">(Server-Side APIs)</p>
-                    </a>
-                </div>
-                <div className="projects"> 
-                    <a className="port-link" href="https://lara-destein13.github.io/my-backyard-trails/">
-                        <h2 className="project-title">My Backyard Trails</h2>
-                        <p className="project-description">(Interactive Front-End Project)</p>
-                    </a>
-                </div>
-                <div className="projects"> 
-                    <a className="port-link" href="https://lara-destein13.github.io/stick-ease/">
-                        <h2 className="project-title">Stick Ease</h2>
-                        <p className="project-description">(Interactive MERN SPA Project)</p>
-                    </a>
-                </div>
+                <a href="https://lara-destein13.github.io/password-generator/">
+                    <div className="photo-wrapper">
+                        <img className="photo" src={password} width="400px" height="400px" />
+                    </div>                    
+                </a>
+                <a href="https://lara-destein13.github.io/4-code-quiz/">
+                    <div className="photo-wrapper">
+                        <img className="photo" src={quiz} width="400px" height="400px" />
+                    </div>                    
+                </a>
+                <a href="https://lara-destein13.github.io/5-work-day-scheduler/">
+                    <div className="photo-wrapper">
+                        <img className="photo" src={scheduler} width="400px" height="400px" />
+                    </div>                    
+                </a>
+                <a href="https://lara-destein13.github.io/weather/">
+                    <div className="photo-wrapper">
+                        <img className="photo" src={weather} width="400px" height="400px" />
+                    </div>                    
+                </a>
+                <a href="https://lara-destein13.github.io/my-backyard-trails/">
+                    <div className="photo-wrapper">
+                        <img className="photo" src={trails} width="400px" height="400px" />
+                    </div>                    
+                </a>
+                <a href="https://lara-destein13.github.io/stick-ease/">
+                    <div className="photo-wrapper">
+                        <img className="photo" src={stick} width="400px" height="400px" />
+                    </div>                    
+                </a>
             </div>
         );
     }
