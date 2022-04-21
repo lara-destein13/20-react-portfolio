ea365be00c76b3dbe320a4089acee6cb2eb576c6
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-04-21 18:40:13 -0400

    update contact me component with links

--- react-portfolio/src/ContactMe.js
+++ react-portfolio/src/ContactMe.js
@@ -19,7 +19,10 @@ class ContactMe extends Component {
                 <h2 className="contact-me-header">Contact Me</h2> 
                 <div className="contact-info">
                     Phone: (925) 819-1194<br></br>
-                    Email: laradestein13@gmail.com
+                    Email: laradestein13@gmail.com<br></br>
+                    LinkedIn: <a href="https://www.linkedin.com/in/lara-destein-6353bb149/">https://www.linkedin.com/in/lara-destein-6353bb149/</a><br></br>
+                    Github: <a href="https://github.com/lara-destein13">https://github.com/lara-destein13</a><br></br>
+                    Facebook: <a href="https://www.facebook.com/lara.destein">https://www.facebook.com/lara.destein</a><br></br>
                 </div>        
             </div>   
         );
