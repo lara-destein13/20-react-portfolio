ef7e5e06cdc74a1fd76bf0fb6b7f3771b92d62ac
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2022-04-21 17:47:13 -0400

    remove contact form and related css

--- react-portfolio/src/ContactMe.js
+++ react-portfolio/src/ContactMe.js
@@ -20,16 +20,7 @@ class ContactMe extends Component {
                 <div className="contact-info">
                     Phone: (925) 819-1194<br></br>
                     Email: laradestein13@gmail.com
-                </div>
-                <form className="Form">
-                    <label for="name" className="label">Enter full name:</label>
-                    <input type="text" className="text-input" />
-                    <label for="email" className="label">Enter email address:</label>
-                    <input type="email" className="email-input" />
-                    <label for="message" className="label">Message:</label>
-                    <textarea type="textarea" className="message-input"></textarea>
-                    <button type="submit"className="message-submit">Submit</button>
-                </form>              
+                </div>        
             </div>   
         );
     }        
